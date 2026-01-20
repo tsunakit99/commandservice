a6012a3ec12bf9a304893da556f7f30b5ee2e5d8
tsunakit99 <tainosuke0926@gmail.com>
2026-01-21 06:45:41 +0900

    feat: DBErrorのハンドリング関数実装

---
+++ infra/sqlboiler/handler/errs.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"errors"
+	"log"
+	"net"
+
+	"github.com/tsunakit99/commandservice/errs"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+// データベースアクセスエラーのハンドリング
+func DBErrHandler(err error) error {
+	var opErr *net.OpError
+	var driverErr *mysql.MySQLError
+	if errors.As(err, &opErr) { // 接続がタイムアウトかネットワーク関連のエラーの場合
+		log.Println(err.Error())
+		return errs.NewInternalError(opErr.Error())
+	} else if errors.As(err, &driverErr) { // MySQLのドライバエラーの場合
+		log.Printf("Code:%d Message:%s\n", driverErr.Number, driverErr.Message)
+		if driverErr.Number == 1062 { // 一意制約違反
+			return errs.NewCRUDError("一意制約違反です。")
+		} else {
+			return errs.NewInternalError(driverErr.Message)
+		}
+	} else { // その他のエラー
+		log.Println(err.Error())
+		return errs.NewInternalError(err.Error())
+	}
+}
